3f831b008682b2b17d55eb721ce10af27f10051c
Ben Challis <ben@lendable.co.uk>
2021-11-19 11:52:35 +0000

    More fix-ups pre open source

--- src/Serializer.php
+++ src/Serializer.php
@@ -16,10 +16,7 @@ final class Serializer
         try {
             $serialized = \json_encode($data, \JSON_THROW_ON_ERROR | \JSON_UNESCAPED_SLASHES | \JSON_UNESCAPED_UNICODE);
         } catch (\JsonException $exception) {
-            $errorCode = $exception->getCode();
-            \assert(\is_int($errorCode));
-
-            throw new SerializationFailed($errorCode, $exception->getMessage(), $exception);
+            throw new SerializationFailed($exception);
         }
 
         return $serialized;
@@ -36,10 +33,7 @@ final class Serializer
         try {
             $data = \json_decode($json, true, 512, \JSON_THROW_ON_ERROR);
         } catch (\JsonException $exception) {
-            $errorCode = $exception->getCode();
-            \assert(\is_int($errorCode));
-
-            throw new DeserializationFailed($errorCode, $exception->getMessage(), $exception);
+            throw new DeserializationFailed($exception);
         }
 
         if (!\is_array($data)) {
