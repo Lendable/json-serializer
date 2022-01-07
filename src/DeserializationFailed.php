891ae1e5797440bfd3e0876cfa40d88b897a9edb
Ben Challis <ben@lendable.co.uk>
2022-01-07 08:38:41 +0000

    Fix-up PHPStan being smarter re: \Throwable::getCode() being an int outside of edge cases

--- src/DeserializationFailed.php
+++ src/DeserializationFailed.php
@@ -8,16 +8,13 @@ final class DeserializationFailed extends \RuntimeException implements Failure
 {
     public function __construct(\JsonException $cause)
     {
-        $causeCode = $cause->getCode();
-        \assert(\is_int($causeCode));
-
         parent::__construct(
             \sprintf(
                 'Failed to deserialize data from JSON. Error code: %d, error message: %s.',
-                $causeCode,
+                $cause->getCode(),
                 $cause->getMessage(),
             ),
-            $causeCode,
+            $cause->getCode(),
             $cause,
         );
     }
--- src/SerializationFailed.php
+++ src/SerializationFailed.php
@@ -8,16 +8,13 @@ final class SerializationFailed extends \RuntimeException implements Failure
 {
     public function __construct(\JsonException $cause)
     {
-        $causeCode = $cause->getCode();
-        \assert(\is_int($causeCode));
-
         parent::__construct(
             \sprintf(
                 'Failed to serialize data to JSON. Error code: %d, error message: %s.',
-                $causeCode,
+                $cause->getCode(),
                 $cause->getMessage(),
             ),
-            $causeCode,
+            $cause->getCode(),
             $cause,
         );
     }
