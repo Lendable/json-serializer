1dccdb01c3015167530cd55c30fd1660931df789
Ben Challis <ben@lendable.co.uk>
2022-12-06 10:54:08 +0000

    Update to require PHP 8.0

--- rector.php
+++ rector.php
@@ -2,9 +2,7 @@
 
 declare(strict_types=1);
 
-use Rector\CodeQuality\Rector\ClassMethod\DateTimeToDateTimeInterfaceRector;
 use Rector\Config\RectorConfig;
-use Rector\Core\Configuration\Option;
 use Rector\Core\ValueObject\PhpVersion;
 use Rector\Set\ValueObject\LevelSetList;
 use Rector\Set\ValueObject\SetList;
@@ -12,16 +10,13 @@ use Rector\Set\ValueObject\SetList;
 return static function (RectorConfig $rectorConfig): void {
     $rectorConfig->parallel();
     $rectorConfig->paths([__DIR__.'/src', __DIR__.'/tests']);
-    $rectorConfig->phpVersion(PhpVersion::PHP_74);
+    $rectorConfig->phpVersion(PhpVersion::PHP_80);
     $rectorConfig->phpstanConfig(__DIR__.'/phpstan-rector.neon');
-    $rectorConfig->skip([
-        DateTimeToDateTimeInterfaceRector::class,
-    ]);
     $rectorConfig->importNames();
-    $rectorConfig->parameters()->set(Option::IMPORT_SHORT_CLASSES, false);
+    $rectorConfig->importShortClasses(false);
 
     $rectorConfig->sets([
         SetList::CODE_QUALITY,
-        LevelSetList::UP_TO_PHP_74,
+        LevelSetList::UP_TO_PHP_80,
     ]);
 };
